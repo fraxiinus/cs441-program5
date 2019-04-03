c75bd9aeb734070f975fb50e5c0abb7e66351dc5
Andrew Lee <andrew1421lee@gmail.com>
2019-04-03 15:18:18 -0400

    tweaking layouts

--- app/src/main/java/com/etirps/zhu/leaguerewind/MainActivity.kt
+++ app/src/main/java/com/etirps/zhu/leaguerewind/MainActivity.kt
@@ -15,6 +15,7 @@ import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import android.view.View
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                     or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
         }
     }
 
